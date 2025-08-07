858496a50c329cd034e29edceaf0f34409e4a32d
JuKoWi <julius.willrich@uni-jena.de>
2025-08-07 19:06:44 +0200

    commutator and simple righthandside added

--- SBE.py
+++ SBE.py
@@ -17,17 +17,17 @@ class BandStructure:
         self.a = angstrom_to_bohr(a)
     
     
-    def _get_H_mat(self, k):
-        H = np.zeros((2,2, len(k)))
+    def get_H_mat(self, k):
+        H = np.zeros((2,2))
         H[0,0] = self.Ec + self.tc * np.cos(k * self.a)
         H[1,1] = self.Ev + self.tv * np.cos(k * self.a)
         return H
     
-    def plot_bands(self, num_k):
+    def plot_bands(self, num_k): # deprecated
         fig, ax = plt.subplots()
         brillzone = np.linspace(-np.pi/self.a, np.pi/self.a, num_k)
-        ax.plot(brillzone, self._get_H_mat(brillzone)[0,0])
-        ax.plot(brillzone, self._get_H_mat(brillzone)[1,1])
+        ax.plot(brillzone, self.get_H_mat(brillzone)[0,0])
+        ax.plot(brillzone, self.get_H_mat(brillzone)[1,1])
         ax.set_xlabel(r"k/$a_0^{-1}$")
         ax.set_ylabel(r"E/$E_h$")
         plt.show()
@@ -45,10 +45,10 @@ class Simulation:
         """t-end in fs"""
         t_end = fs_to_au(t_end)
         self.time = np.linspace(0, t_end, n_steps)
-    
-    def make_step(self):
-        pass
 
+    def define_bands(self, Ec, Ev, tc, tv):
+        self.bands = BandStructure(Ec=Ec, Ev=Ev, tc=tc, tv=tv, a=self.a)
+    
     def define_pulse(self, sigma, lam, t_start, Enull):
         """lam in nm, sigma in fs, Enull in V/m"""
         self.t_start = fs_to_au(t_start)
@@ -56,19 +56,56 @@ class Simulation:
         lam = nm_to_au(lam)
         self.omega = lam_to_omega(lam) 
         self.E_null = Vpm_to_au(Enull)
-        self.E_field = self.E_null * np.sin(self.omega * self.time) #* np.exp(-(self.time - self.t_start)**2 / (2 * self.sigma**2) )
+        self.E_field = self.E_null * np.sin(self.omega * self.time) * np.exp(-(self.time - self.t_start)**2 / (2 * self.sigma**2) )
 
     def get_vector_potential(self):
         f = lambda t, y: gaussian_sine(t, self.omega, self.sigma, self.t_start, self.E_null)
-        solution = solve_ivp(f, (self.time[0], self.time[-1]), [0], t_eval=self.time, rtol=1e-10, atol=1e-12)
-        print(solution.nfev)
+        solution = solve_ivp(f, (self.time[0], self.time[-1]), [0], t_eval=self.time,method='DOP853', 
+                             rtol=1e-10, 
+                             atol=1e-12
+                             )
         self.A_field = solution.y[0]
 
     def define_system(self, num_k, a):
         """a in angstrom"""
+        self.num_k = num_k
         self.a = angstrom_to_bohr(a)
         self.k_list = np.linspace(-np.pi/a, np.pi/a, num_k)
-        self.dens_mat = DensityMatrix(self.k_list, a, self.time)
+        self.mat_init = np.zeros((len(self.k_list), 2, 2))
+        self.mat_init[:,0,0] = 1 # fully populate conduction band
+
+    def set_h_null(self, dipole_element):
+        h_null = np.zeros((len(self.k_list), 2, 2))
+        for i,k in enumerate(self.k_list):
+            h_null[i] = self.bands.get_H_mat(k)
+        h_null[:,1,0] = dipole_element
+        h_null[:,0,1] = dipole_element
+        h_null = h_null.flatten()
+        self.h_null = h_null
+
+    def commute(self, rho):
+        H = np.reshape(self.h_null, (len(self.k_list), 2, 2)) 
+        rho = np.reshape(rho, (len(self.k_list), 2, 2)) 
+        commutator = np.einsum('ijk, ikl -> ijl', H,rho) - np.einsum('ijk, ikl -> ijl', rho, H)
+        # commutator = H @ rho - rho @ H 
+        return commutator.flatten()
+
+    def get_rhs(self,t, rho):
+        rhs = self.commute(rho) #h_null is constant with time
+        return rhs.flatten()
+
+    def E_k_function(self, t):
+        E = self.E_null * np.sin(self.omega * t) * np.exp(-(t - self.t_start)**2 / (2 * self.sigma**2) )
+        E_k = E * self.k_list
+        E_k = np.repeat(E_k, 4)
+        return E_k
+
+    def integrate(self):
+        """needs self.rhs"""
+        solution = solve_ivp(self.get_rhs, t_span=(self.time[0], self.time[-1]), y0=self.mat_init.flatten(), t_eval=self.time)
+        print(np.shape(solution.t))
+        self.solution = np.reshape(solution.y, (len(self.time),len(self.k_list), 2,2))
+        print(np.shape(self.solution))
 
     def plot_field_E(self):
         fig, ax = plt.subplots()
@@ -89,36 +126,42 @@ class Simulation:
         ax.set_ylabel(r"A/$Vsm^{-1}$")
         plt.show()
 
-class DensityMatrix:
-
-    def __init__(self, k_list, a, time):
-        self.k_list = k_list
-        self.a = a
-        self.time = time
-        self.mat = np.zeros((len(time), len(k_list), 2, 2))
-        self.mat[0,:,0,0] = 1 # fully populate conduction band
-
-    def get_k_deriv(self):
-        deriv = np.roll
+    def plot_density_matrix(self, k):
+        fig, ax = plt.subplots()
 
 
 def gaussian_sine(t, omega, sigma, t_start, E_null):
-    return -E_null * np.sin(omega * t) #* np.exp(-(t- t_start)**2 / (2 * sigma**2) )
+    return -E_null * np.sin(omega * t) * np.exp(-(t- t_start)**2 / (2 * sigma**2) )
 
 
 if __name__ =="__main__":
-    ZnO = BandStructure(Ec=4, Ev=-3, tc=-1.5, tv=0.5, a=2) #5.16
-    # ZnO.plot_bands(num_k=40)
-    sim = Simulation(t_end=30, n_steps=10000)
+    sim = Simulation(t_end=30, n_steps=1000)
     sim.define_pulse(sigma=3, lam=774, t_start=11, Enull=1)
     sim.define_system(num_k=100, a=9.8) 
-    sim.get_vector_potential()
-    sim.plot_field_E()
-    sim.plot_field_A()
+    sim.define_bands(Ec=4, Ev=-3, tc=-1.5, tv=0.5)
+    sim.set_h_null(dipole_element=5)
+    sim.integrate() 
+
     
 
-# vectorpotential from E
 # independently: write circularly polarized light
 # solve SBE ode integrate
 # equidistant time steps
 # nyquist theorem for time step estimation delta e delta t roundabout hbar
+"""
+write laser as function return 4*k 1D array that multiplies with respective k
+write dens-mat as k,4, only initial
+write function that
+    reshapes flat array into matrix shape
+    commutes rho and H_k
+    flattens
+write function that
+    reshapes flat array into matrix shape
+    calculates periodic k-derivative
+    flattens
+"""
+"""
+possible tests:
+    leave out field: only constant matrix elements
+
+"""
