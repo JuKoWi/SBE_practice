fixed unit conversion, no flattening between elementary steps

SBE.py
@@ -24,15 +24,6 @@ class BandStructure:
         H[1,1] = self.Ev + self.tv * np.cos(k * self.a)
         return H
     
-    def plot_bands(self, num_k): # deprecated
-        fig, ax = plt.subplots()
-        brillzone = np.linspace(-np.pi/self.a, np.pi/self.a, num_k)
-        ax.plot(brillzone, self.get_H_mat(brillzone)[0,0])
-        ax.plot(brillzone, self.get_H_mat(brillzone)[1,1])
-        ax.set_xlabel(r"k/$a_0^{-1}$")
-        ax.set_ylabel(r"E/$E_h$")
-        plt.show()
-
 """objective: get density matrix at times t0 to t
 diffential equation formulated for each k coupled by k-gradient
 all matrices written in abstract two level basis
@@ -58,7 +49,7 @@ class Simulation:
         lam = nm_to_au(lam)
         self.omega = lam_to_omega(lam) 
         self.E0 = Vpm_to_au(E0)
-        self.E_field= gaussian_sine(t=self.time, omega=self.omega, sigma=self.sigma, t_start=self.t_start, E0=self.E0)
+        self.E_field = np.array([gaussian_sine(t=t, omega=self.omega, sigma=self.sigma, t_start=self.t_start, E0=self.E0) for t in self.time])
         
 
     def define_system(self, num_k, a):
@@ -66,91 +57,58 @@ class Simulation:
         self.num_k = num_k
         self.a = angstrom_to_bohr(a)
         self.k_list = np.linspace(-np.pi/self.a, np.pi/self.a, num_k, endpoint=False)
-        mat_init = np.zeros((len(self.k_list), 2, 2))
-        mat_init[:,1,1] = 1 # fully populate valence band
-        self.mat_init = mat_init.flatten()
+        self.mat_init = np.zeros((self.num_k, 2, 2), dtype='complex')
+        self.mat_init[:,1,1] = 1 # fully populate valence band
 
     def set_H_constant(self, dipole_element):
         dipole_element = Cm_to_au(dipole_element)
-        h_const = np.zeros((len(self.k_list), 2, 2))
+        self.h_const = np.zeros((self.num_k, 2, 2))
         for i,k in enumerate(self.k_list):
-            h_const[i] = self.bands.get_H_mat(k)
+            self.h_const[i] = self.bands.get_H_mat(k)
         self.dipole_mat = np.zeros((2,2))
         self.dipole_mat[0,1] = dipole_element  # not part of H_null but constant over time
         self.dipole_mat[1,0] = dipole_element  # not part of H_null but constant over time
-        h_const = h_const.flatten()
-        self.h_const = h_const
     
     def get_H(self, t):
-        h_const = np.reshape(self.h_const, (self.num_k, 2,2))
-        E_mat = np.reshape(self.E_function(t=t), (self.num_k, 2,2))
-        h_mat = E_mat * self.dipole_mat + h_const
-        return h_mat.flatten()
+        # h_mat = np.repeat(np.expand_dims(self.get_E(t) * self.dipole_mat, axis=0), self.num_k, axis=0) + self.h_const
+        h_mat = self.get_E(t) * self.dipole_mat + self.h_const
+        return h_mat
+
+    def get_E(self, t):
+        E = gaussian_sine(t, omega=self.omega, sigma=self.sigma, t_start=self.t_start, E0=self.E0)
+        return E
 
     def commute(self, rho, t):
-        H = np.reshape(self.get_H(t), (len(self.k_list), 2, 2)) 
-        rho = np.reshape(rho, (len(self.k_list), 2, 2))
-        commutator = np.einsum('ijk, ikl -> ijl', H,rho) - np.einsum('ijk, ikl -> ijl', rho, H)
-        # commutator = H @ rho - rho @ H 
-        return commutator.flatten()
-
-    def get_rhs(self,t, rho):
-        rho = self.y_to_rho(rho)
-        E = gaussian_sine(t, omega=self.omega, sigma=self.sigma, t_start=self.t_start, E0= self.E0) 
-        rhs = -1j * self.commute(rho, t) + E * self.get_k_partial(rho) #h_null is constant with time
-        rhs = self.rho_to_y(rhs)
-        print(f"rhs executed at {au_to_fs(t)}")
-        print(E)
-
-        return rhs 
-
-    def E_function(self, t):
-        """flat array of dim num_k to introduce the E-field for every k"""
-        E = gaussian_sine(t, omega=self.omega, sigma=self.sigma, t_start=self.t_start, E0=self.E0)
-        E = np.repeat(E, self.num_k)
-        E_mat = np.zeros((self.num_k, 2,2))
-        E_mat[:,1,0] = E
-        E_mat[:,0,1] = E
-        return E_mat.flatten()
+        H = self.get_H(t) 
+        # commutator = np.einsum('ijk, ikl -> ijl', H,rho) - np.einsum('ijk, ikl -> ijl', rho, H)
+        commutator = H @ rho - rho @ H 
+        return commutator
 
     def get_k_partial(self, rho):
-        rho = np.reshape(rho, (len(self.k_list), 2, 2)) 
         xplush = np.roll(rho, shift=-1, axis=0)
         xminush = np.roll(rho, shift=1, axis=0)
         xplustwoh = np.roll(rho, shift=-2, axis=0)
         xminustwoh = np.roll(rho, shift=2, axis=0)
         h = self.k_list[1] - self.k_list[0]
         deriv = (8 * xplush - 8 * xminush + xminustwoh - xplustwoh)/(12 * h)
-        return deriv.flatten()
+        return deriv
+
+    def get_rhs(self,t, y):
+        rho = self.y_to_rho(y)
+        E = self.get_E(t) 
+        rhs = 1j*self.commute(rho, t) + E * self.get_k_partial(rho) 
+        return self.rho_to_y(rhs) 
 
     def integrate(self):
         """needs self.rhs"""
-        solution = solve_ivp(lambda t, rho : self.get_rhs(t=t, rho=rho), t_span=(self.time[0], self.time[-1]), y0=self.rho_to_y(self.mat_init), t_eval=self.time,
-                            # method='BDF',
-                            # atol=1e-14, rtol=1e-14
+        solution = solve_ivp(lambda t, y : self.get_rhs(t=t, y=y), t_span=(self.time[0], self.time[-1]), y0=self.rho_to_y(self.mat_init), t_eval=self.time,
+                            # method='BDF', 
+                            # atol=1e-12, rtol=1e-12,
+                            # atol = np.array([1e-10, 1e-10, 1e-30, 1e-30, 1e-30, 1e-30, 1e-10, 1e-10])
                             )
-        print(np.shape(solution.y))
-        print(solution.status)
         rho_time = solution.y.T # transpose to switch time and other dimensions
-        real = rho_time[:,:self.num_k * 4, ...]
-        im = rho_time[:,self.num_k * 4 :, ...]
-        rho_time = real + 1j*im
-        self.solution = np.reshape(rho_time, (len(self.time),len(self.k_list), 2,2))
-
-    def plot_field_E(self):
-        fig, ax = plt.subplots()
-        time = au_to_fs(self.time)
-        E = au_to_Vpm(self.E_field)
-        ax.plot(time, E)
-        ax.set_xlabel("t/fs")
-        ax.set_ylabel(r"E/$Vm^{-1}$")
-        plt.show()
+        self.solution = np.array([self.y_to_rho(rho_time[i]) for i in range(rho_time.shape[0])])
     
-    def plot_density_matrix(self, k_index):
-        fig, ax = plt.subplots()
-        ax.plot(au_to_fs(self.time), np.abs(self.solution[:,k_index,1,0])) 
-        plt.show()
-
     def get_vector_potential(self):
         f = lambda t, y: gaussian_sine(t, self.omega, self.sigma, self.t_start, self.E0)
         solution = solve_ivp(f, (self.time[0], self.time[-1]), [0], t_eval=self.time,
@@ -171,26 +129,39 @@ class Simulation:
         plt.show()
     
     def y_to_rho(self, y):
-        # return (y[::2] + 1j * y[1::2]).reshape(rhso.shape)
-        real = y[:self.num_k*4]
-        im = y[self.num_k*4:]
-        rho = real + 1j * im
-        return rho
+        """takes matrix shape and returns flat shape with double the length"""
+        return np.reshape(y[::2] + 1j * y[1::2], np.shape(self.mat_init))
     
     def rho_to_y(self, rho):
-        # y = np.empty(rho.size *2)
-        # y[::2] = np.real().flatten()
-
-        real = np.real(rho)
-        im = np.imag(rho)
-        y = np.append(real, im, axis=0) 
+        y = np.zeros(rho.size *2)
+        y[::2] = np.real(rho).flatten()
+        y[1::2] = np.imag(rho).flatten()
         return y
+    
+    def check_idempotence(self):
+        test_array = np.empty(np.shape(self.mat_init), dtype='complex')
+        result = self.y_to_rho(self.rho_to_y(test_array))
+        print(np.array_equal(test_array, result))
 
+
+class Field:
+    """must include a method with t as input"""
+
+    def __init__(self):
+        pass
+
+
+class Plot:
+
+    def __init__(self, simulation):
+        self.time = simulation.time
+        self.solution = simulation.solution
+        self.k_list = simulation.k_list
+        self.E_field =simulation.E_field
+    
     def get_heatmap_rho(self):
         rho = np.abs(self.solution)
         fig, axs = plt.subplots(3,1, figsize=(8,4), sharex=True)
-        # im1 = axs[0].pcolormesh(au_to_fs(self.time), self.k_list, (rho[:,:,0,0]).T, shading='auto')
-        # im2 = axs[1].pcolormesh(au_to_fs(self.time), self.k_list, (rho[:,:,1,1]-rho[0,:,1,1]).T, shading='auto')
         im1 = axs[0].pcolormesh(au_to_fs(self.time), self.k_list, rho[:,:,0,1].T, shading='auto')
         im2 = axs[1].pcolormesh(au_to_fs(self.time), self.k_list, rho[:,:,1,1].T, shading='auto')
         axs[2].plot(au_to_fs(self.time), au_to_Vpm(self.E_field))
@@ -203,46 +174,49 @@ class Simulation:
         fig.colorbar(im1, ax=axs[0], orientation='horizontal')
         fig.colorbar(im2, ax=axs[1], orientation='horizontal')
         plt.show()
+    
+    def plot_field_E(self):
+        fig, ax = plt.subplots()
+        time = au_to_fs(self.time)
+        E = np.array([au_to_Vpm(self.get_E(t)) for t in self.time])
+        ax.plot(time, E)
+        ax.set_xlabel("t/fs")
+        ax.set_ylabel(r"E/$Vm^{-1}$")
+        plt.show()
 
+    def plot_density_matrix(self, k_index):
+        fig, ax = plt.subplots()
+        ax.plot(au_to_fs(self.time), np.abs(self.solution[:,k_index,1,1])) 
+        plt.show()
 
 
 def gaussian_sine(t, omega, sigma, t_start, E0):
-    return -E0 * np.sin(omega * t) #* np.exp(-(t- t_start)**2 / (2 * sigma**2) )
+    return -E0 * np.sin(omega * t) * np.exp(-(t- t_start)**2 / (2 * sigma**2) )
 
 
 if __name__ =="__main__":
-    sim = Simulation(t_end=100, n_steps=1000)
-    sim.define_pulse(sigma=10, lam=2000, t_start=40, E0=1e9) #E_0 = 1e11 roundabout corresponding to I = 1.5e14 W/cm^2
-    sim.define_system(num_k=50, a=9.8) 
+    sim = Simulation(t_end=90, n_steps=1000)
+    sim.define_pulse(sigma=5, lam=774, t_start=50, E0=1e9) #E_0 = 1e11 roundabout corresponding to I = 1.5e14 W/cm^2
+    sim.define_system(num_k=100, a=9.8) 
     sim.define_bands(Ec=4, Ev=-3, tc=-1.5, tv=0.5)
-    sim.set_H_constant(dipole_element=9e-29) # corresponds to roundabout 9 a.u.
+    sim.set_H_constant(dipole_element=9e-29) # 9e-29 corresponds to roundabout 9 a.u.
+    # sim.plot_field_E()
     sim.integrate() 
-    sim.plot_field_E()
-    sim.get_heatmap_rho()
+    results = Plot(sim)
+    results.get_heatmap_rho()
+    # sim.plot_density_matrix(k_index=0)
 
     
     
 
     
-
-# independently: write circularly polarized light
-# solve SBE ode integrate
-# equidistant time steps
-# nyquist theorem for time step estimation delta e delta t roundabout hbar
-# write definiton vector potential based on E0, ignore envelope
-# chekc for k dependence of simulation
-# sum diagonal elements over k (integrate) plot over time, norm to electrons / volume cell
-# calculate current
-
 """
-write laser as function return 4*k 1D array that multiplies with respective k
-write dens-mat as k,4, only initial
-write function that
-    reshapes flat array into matrix shape
-    commutes rho and H_k
-    flattens
-write function that
-    reshapes flat array into matrix shape
-    calculates periodic k-derivative
-    flattens
+independently: write circularly polarized light
+solve SBE ode integrate
+equidistant time steps
+nyquist theorem for time step estimation delta e delta t roundabout hbar
+write definiton vector potential based on E0, ignore envelope
+chekc for k dependence of simulation
+sum diagonal elements over k (integrate) plot over time, norm to electrons / volume cell
+calculate current
 """

tests.py
@@ -22,14 +22,14 @@ def check_rho_zero_derivative():
     sim.define_pulse(sigma=3, lam=774, t_start=11, E0=1)
     sim.define_system(num_k=100, a=9.8) 
     sim.define_bands(Ec=4, Ev=-3, tc=-1.5, tv=0.5)
-    mat_init = np.reshape(sim.mat_init, (sim.num_k, 2,2))
+    mat_init = sim.mat_init
     plt.plot(sim.k_list, mat_init[:,0,0], label='0,0')
     plt.plot(sim.k_list, mat_init[:,1,0], label='1,0')
     plt.plot(sim.k_list, mat_init[:,0,1], label='0,1')
     plt.plot(sim.k_list, mat_init[:,1,1], label='1,1')
     plt.legend()
     plt.show()
-    deriv = np.reshape(sim.get_k_partial(sim.mat_init), (sim.num_k, 2,2))
+    deriv = sim.get_k_partial(sim.mat_init)
     plt.plot(sim.k_list, deriv[:, 0, 0], label='0,0')
     plt.plot(sim.k_list, deriv[:, 1, 0], label='1,0')
     plt.plot(sim.k_list, deriv[:, 0, 1], label='0,1')
@@ -42,8 +42,8 @@ def plot_H_const():
     sim.define_pulse(sigma=3, lam=774, t_start=11, E0=1)
     sim.define_system(num_k=100, a=9.8) 
     sim.define_bands(Ec=4, Ev=-3, tc=-1.5, tv=0.5)
-    sim.set_H_constant(dipole_element=9e-29)
-    h_const = np.reshape(sim.h_const, (sim.num_k, 2,2))
+    sim.set_H_constant(dipole_element=1e11)
+    h_const = sim.h_const
     plt.plot(sim.k_list, h_const[:,0,0], label='0,0')
     plt.plot(sim.k_list, h_const[:,1,0], label='1,0')
     plt.plot(sim.k_list, h_const[:,0,1], label='0,1')
@@ -57,9 +57,8 @@ def plot_H_deriv():
     sim.define_system(num_k=100, a=9.8) 
     sim.define_bands(Ec=4, Ev=-3, tc=-1.5, tv=0.5)
     sim.set_H_constant(dipole_element=9e-29)
-    h_const = np.reshape(sim.h_const, (sim.num_k, 2,2))
-    deriv_flat = sim.get_k_partial(sim.h_const)
-    deriv = np.reshape(deriv_flat, (sim.num_k, 2,2))
+    h_const = sim.h_const 
+    deriv = sim.get_k_partial(sim.h_const)
     plt.plot(sim.k_list, deriv[:,0,0], label='0,0')
     plt.plot(sim.k_list, deriv[:,1,0], label='1,0')
     plt.plot(sim.k_list, deriv[:,0,1], label='0,1')
@@ -76,7 +75,7 @@ def plot_E_field():
     time = sim.time
     E_mat = np.zeros((len(sim.time), sim.num_k, 2,2))
     for i,t in enumerate(sim.time):
-        E_mat[i] = np.reshape(sim.E_function(t),(sim.num_k, 2, 2))
+        E_mat[i] = np.reshape(sim.get_E(t),(sim.num_k, 2, 2))
     plt.plot(time, E_mat[:,0,0,1]) 
     plt.show()
 
@@ -113,4 +112,3 @@ def check_trace_const():
     print(np.all(traces == traces[0]))
 
 check_trace_const()
-    

unit_conversion.py
@@ -2,23 +2,21 @@ import scipy.constants as constants
 import numpy as np
 
 def eV_to_au(eV):
-    return eV / constants.physical_constants['Hartree energy in eV'][-1]
- 
+    return eV / constants.physical_constants['Hartree energy in eV'][0]
+
 
 def au_to_ev(au):
-    return au *constants.physical_constants['Hartree energy in eV'][-1]
+    return au *constants.physical_constants['Hartree energy in eV'][0]
     
 def angstrom_to_bohr(val):
-    bohr_radius_in_meters = constants.physical_constants['Bohr radius'][-1]  # in meters
+    bohr_radius_in_meters = constants.physical_constants['Bohr radius'][0]  # in meters
     angstrom_in_meters = constants.angstrom  # 0 Å = 1e-10 m
     return val * angstrom_in_meters / bohr_radius_in_meters
 
 def bohr_to_angstrom(val):
-    bohr_radius_in_meters = constants.physical_constants['Bohr radius'][-1]  # in meters
+    bohr_radius_in_meters = constants.physical_constants['Bohr radius'][0]  # in meters
     angstrom_in_meters = constants.angstrom
     return val * bohr_radius_in_meters / angstrom_in_meters
-
-
 
 def nm_to_au(wavelength_nm):
     bohr_radius_m = constants.physical_constants['Bohr radius'][0]  # in meters